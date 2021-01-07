150019f47f74139972095d095325e414e52fe3a0
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-01-07 20:20:19 -0300

    some comments

--- creational/factories/factory-method/factory_method.py
+++ creational/factories/factory-method/factory_method.py
@@ -8,21 +8,26 @@ class Veihcle(ABC):
 	@abstractmethod
 	def search_client(self) -> None: pass
 
+
 class LuxCar(Veihcle):
 	def search_client(self) -> None:
 		print('Lux Car is looking for the client')
+
 
 class PopularCar(Veihcle):
 	def search_client(self) -> None:
 		print('Popular Car is looking for the client')
 
+
 class PopularMotorcycle(Veihcle):
 	def search_client(self) -> None:
 		print('Popular Motorcycle is looking for the client')
 
+
 class LuxMotorcycle(Veihcle):
 	def search_client(self) -> None:
 		print('Lux Motorcycle is looking for the client')
+
 
 class VehicleFactory(ABC):
 	"""
@@ -33,17 +38,34 @@ class VehicleFactory(ABC):
 
 	@staticmethod
 	@abstractmethod
-	def get_vehicle(veihcle_type: str) -> Veihcle: pass
+	def get_vehicle(veihcle_type: str) -> Veihcle:
+		"""
+		The get vehicle will be implemented and overrided
+		by the subclasses of VehicleFactory
+		"""
+		pass
 
 	def search_client(self) -> None:
+		"""
+		This is the wrapper which envolves the
+		search_client() method of the
+		Vehicle object
+		This is the same of the simple factory 2
+		"""
 		self.vehicle.search_client()
+
 
 class Afiliated01VehicleFactory(VehicleFactory):
 	"""
 	Afiliated 01 dicide how objects can be created
 	"""
+
 	@staticmethod	
 	def get_vehicle(veihcle_type: str) -> Veihcle:
+		"""
+		Overrides the get_vehicle() method
+		of the Vehicle factory
+		"""
 		if veihcle_type == 'LuxCar':
 			return LuxCar()
 		if veihcle_type == 'PopularCar':
@@ -54,12 +76,19 @@ class Afiliated01VehicleFactory(VehicleFactory):
 			return LuxMotorcycle()
 		assert 0, 'Vehicle does not exist'
 
+
 class Afiliated02VehicleFactory(VehicleFactory):
 	"""
 	Afiliated 02 dicide how objects can be created
 	"""
+
 	@staticmethod	
 	def get_vehicle(veihcle_type: str) -> Veihcle:
+		"""
+		Overrides the get_vehicle() method
+		of the Vehicle factory
+		"""
+
 		if veihcle_type == 'PopularCar':
 			return PopularCar()
 		assert 0, 'Vehicle does not exist'
